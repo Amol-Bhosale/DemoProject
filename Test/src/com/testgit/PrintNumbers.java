31e220020e76eaf28a3dff16a171d1708a693519
Amol <bhosale9444@gmail.com>
2023-10-12 09:00:02 +0530

    second commit

--- Test/src/com/testgit/PrintNumbers.java
+++ Test/src/com/testgit/PrintNumbers.java
@@ -3,7 +3,10 @@ package com.testgit;
 public class PrintNumbers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		for (int i=1;i<=11;i++) {
+			System.out.println(i);
+		}
 
 	}
 
